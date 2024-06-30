dc927723d29b6359cceed8bb30194e5dab329271
yunah <favoryun12@gmail.com>
2024-07-01 00:45:32 +0900

    KAN-1 Feat/intro 기본 폴더 구조 세팅

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>홈 페이지</h1>
+      <p>이 페이지는 로그인 전에 보입니다.</p>
+    </main>
+  );
+}
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AfterLoginLayout({children}: any) {
+  return (
+    <div lang="en">
+      애프터 로그인 레이아웃 확인
+     <div>{children}</div>
+    </div>
+  );
+}
