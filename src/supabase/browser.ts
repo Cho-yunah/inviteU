39664838ddfa39265bfccca2d270cf508f8cb423
nahyunbak <kirina@composable.finance>
2024-07-03 21:20:48 +0900

    chore: remove unused file

--- src/supabase/browser.ts
+++ src/supabase/browser.ts
@@ -1,10 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr';
-import { Database } from './supabase';
+import { createBrowserClient } from '@supabase/ssr'
 export function supabaseBrowser() {
-	return createBrowserClient<Database>(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-	);
+  //TODO: add sheme for datapase instead of any
+  return createBrowserClient<any>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  )
 }
 
-export const supabase = supabaseBrowser();
+export const supabase = supabaseBrowser()
