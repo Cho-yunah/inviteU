3a108cf47748eff3e22c295984909d9a9e0fa47d
yunah <favoryun12@gmail.com>
2024-08-28 14:55:43 +0900

    feat/image, video 업로드 컴포넌트 수정

--- src/app/_components/Edit/ContentsInfo/VideoContainer/VideoUpload.tsx
+++ src/app/_components/Edit/ContentsInfo/VideoContainer/VideoUpload.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { Input } from '@/components/ui/input'
 import { IoClose } from "react-icons/io5";
 import { Button } from "@/components/ui/button";
-import { LoadingSpinner } from "@/app/_components/common/LoadingSpinner";
+// import { LoadingSpinner } from "@/app/_components/common/LoadingSpinner";
 
 
 interface FileUploadProps  {
---
+++ src/app/_types/editTypes.ts
@@ -0,0 +1,4 @@
+export interface ContainerProps {
+    setComponents: (component: any) => void;
+    id: string;
+}
