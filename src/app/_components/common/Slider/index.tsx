d50e04c555397463466797f185b6bfcfe8da1984
yunah <favoryun12@gmail.com>
2024-08-30 22:53:07 +0900

    fix/edit page background component 수정

--- src/app/_components/common/Slider/index.tsx
+++ src/app/_components/common/Slider/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -14,19 +14,43 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 interface slideProps {
   slideArr?: Array<{id: number, title: string, color: string, url: any}>
-  // slideArr?: Array<{id: number, title: string, color: string, url: string}>
+  checkedSlide: number;
+  setCheckedSlide: any;
 }
 
-export default function Slider({slideArr}: slideProps) {
-  console.log(slideArr)
+export default function Slider({slideArr, checkedSlide, setCheckedSlide}: slideProps) {
+  const [checked, setChecked] = useState(false)
+  const [activeSlide, setActiveSlide] = useState(0)
+    
+    const onSlideChange = (swiper: any) => {
+      setActiveSlide(swiper.activeIndex+1)
+      // console.log(checkedSlide, activeSlide)
+    }
 
+    const checkHandler = (e: any) => {
+      // console.log(e.target.checked)
+      if(e.target.checked) {
+        setCheckedSlide(activeSlide)
+        setChecked(true)
+      }
+      else setCheckedSlide(0)
+    }
 
   return (
     <>
-      <Swiper pagination={{clickable: true}} modules={[Pagination, Navigation]} className="mySwiper">
+      <div className=''>
+        <input type="checkbox" id="check" checked={checked && checkedSlide==activeSlide? true: false} onChange={checkHandler}/>
+        <label htmlFor="check" className="absolute left-[300px]"></label>
+      </div>
+      <Swiper 
+        pagination={{clickable: true}} 
+        modules={[Pagination, Navigation]} 
+        className="mySwiper"  
+        onSlideChange={onSlideChange}
+      >
         {slideArr?.map((slide) => ( 
-          <SwiperSlide key={slide.id}>
-            <div className={`p-0 shadow-md flex justify-center`}>
+          <SwiperSlide key={slide.id} >
+            <div className={`p-0 shadow-md flex justify-center ${checkedSlide==slide.id && 'shadow-[0_0px_3px_5px_rgba(135,211,248,0.5)]'}`}>
                 <Image src={slide.url} alt={slide.title} width={250} height={650} />
             </div>
           </SwiperSlide>
@@ -36,11 +60,3 @@ export default function Slider({slideArr}: slideProps) {
     </>
   );
 }
-
-
-const slideInfo = [
-  {id: 1, title: 'Slide 1', color: 'bg-blue-100', url: ''},
-  {id: 2, title: 'Slide 2', color: 'bg-green-100', url: ''},
-  {id: 3, title: 'Slide 3', color: 'bg-red-100', url: ''},
-  {id: 4, title: 'Slide 4', color: 'bg-yellow-100', url: ''},
-]
