db833f526ca14aaafc86e49ea6d94a41b374730f
yunah <favoryun12@gmail.com>
2024-10-04 16:43:37 +0900

    feat/map container 상태 올리기 구현

--- src/app/_components/Edit/ContentsInfo/MapContainer/SearchPostcode.tsx
+++ src/app/_components/Edit/ContentsInfo/MapContainer/SearchPostcode.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import DaumPostcode from 'react-daum-postcode';
+import React, { useEffect } from 'react'
+import DaumPostcode, { Address } from 'react-daum-postcode'
 
 interface SearchPostCodeProps {
-    showModal: boolean;
-    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-    address: string;
-    setAddress: React.Dispatch<React.SetStateAction<string>>;
+  showModal: boolean
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+  address: string
+  setAddress: React.Dispatch<React.SetStateAction<string>>
+  postNumber: string
+  setPostNumber: React.Dispatch<React.SetStateAction<string>>
 }
 
-const SearchPostCode = ({showModal, setShowModal, address, setAddress}: SearchPostCodeProps) => {
+const SearchPostCode = ({
+  setShowModal,
+  address,
+  setAddress,
+  setPostNumber,
+}: SearchPostCodeProps) => {
+  const handleComplete = (data: Address) => {
+    let fullAddress = data.address
+    let bname = data.bname
+    let buildingName = data.buildingName
 
-	const handleComplete = (data: { address: string; bname: string; buildingName: string; }) => {
-        let fullAddress = data.address;
-        let bname = data.bname;
-        let buildingName = data.buildingName;
+    console.log(`${fullAddress} (${bname}, ${buildingName})`) // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    setAddress(`${fullAddress} (${bname}, ${buildingName})`)
+    setShowModal(false)
+    setPostNumber(data.zonecode)
+  }
 
-        console.log(`${fullAddress} (${bname}, ${buildingName})`); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
-        setAddress(`${fullAddress} (${bname}, ${buildingName})`);
-        setShowModal(false);
-    };
-
-	return (
-        <div  className='fixed bg-slate-500/35 size-full z-10 left-0 top-0' >
-            <DaumPostcode autoClose onComplete={handleComplete} />
-        </div>
-	)
+  return (
+    <div className="fixed bg-slate-500/35 size-full z-10 left-0 top-0">
+      <DaumPostcode autoClose onComplete={handleComplete} />
+    </div>
+  )
 }
 
-export default SearchPostCode;
+export default SearchPostCode
