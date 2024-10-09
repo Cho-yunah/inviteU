fd2cde6af828126615f4a1f834959cf4fb73c1d6
yunah <favoryun12@gmail.com>
2024-10-10 00:39:18 +0900

    fix/tab 전환시에도 상태 유지를 위한 Image Container 수정

--- src/app/_components/Edit/ContentsInfo/ImagesContainer/FileUpload.tsx
+++ src/app/_components/Edit/ContentsInfo/ImagesContainer/FileUpload.tsx
@@ -1,158 +1,170 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
-import { Button } from "@/components/ui/button";
-import { IoClose } from "react-icons/io5";
-import { useUser } from "@supabase/auth-helpers-react";
+import { Button } from '@/components/ui/button'
+import { IoClose } from 'react-icons/io5'
+import { useUser } from '@supabase/auth-helpers-react'
 
-interface FileUploadProps  {
-    onFileUpload: (files: any) => void;
+interface FileUploadProps {
+  onFileUpload: (files: any) => void
+  imageUrls?: string // Update the type to string
 }
-  
-export const FileUpload: React.FC<FileUploadProps> = ({ onFileUpload, ...props }) => {
-    const {id} = useUser() || {};
 
-    const [filesUrl, setFilesUrl] = useState<string[]>([]);
-    const [isDragging, setIsDragging] = useState(false);
-    const [uploadProgress, setUploadProgress] = useState<number | null>(null);
-    const [filePreviews, setFilePreviews] = useState<string[]>([]);
-  
-    const handleDragEnter = (e: React.DragEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setIsDragging(true);
-    };
-  
-    const handleDragLeave = (e: React.DragEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setIsDragging(false);
-    };
-  
-    const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setIsDragging(false);
-        
-        const files = e.dataTransfer.files;
-      if (files.length > 0) {
-        uploadFiles(files);
-        previewFiles(files);
-      }
-    };
-  
-    const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const files = e.target.files;
-      if (files && files.length > 0) {
-        uploadFiles(files);
-        previewFiles(files);
-      }
-    };
-  
-    const uploadFiles = (files: FileList) => {
-      Array.from(files).forEach((file) => {
-        const totalSize = file.size;
-        const chunkSize = 1024 * 1024; 
-        let uploaded = 0;
-  
-        const uploadInterval = setInterval(() => {
-          uploaded += chunkSize;
-          const progress = (uploaded / totalSize) * 100;
-          setUploadProgress(progress);
-  
-          if (progress >= 100) {
-            clearInterval(uploadInterval);
-            setTimeout(() => {
-              setUploadProgress(null);
-            }, 500); // Delay for visual feedback
-          }
-          // onFileUpload(files);
-          handleFileUpload(file);
-        }, 500); // Simulated 500ms delay per chunk
-      });
-    };
-  
-    const previewFiles = (files: FileList) => {
-      const previews: string[] = [];
-      Array.from(files).forEach((file) => {
-        const reader = new FileReader();
-        reader.onload = () => {
-          previews.push(reader.result as string);
-          setFilePreviews([...filePreviews , ...previews]);
-        };
-        reader.readAsDataURL(file);
-      });
-    };
-  
-    const handleDeletePreview = (index: number) => {
-      const updatedPreviews = [...filePreviews];
-      updatedPreviews.splice(index, 1);
-      setFilePreviews(updatedPreviews);
+export const FileUpload: React.FC<FileUploadProps> = ({ onFileUpload, imageUrls, ...props }) => {
+  const { id } = useUser() || {}
 
-      filesUrl.splice(index, 1);
-      onFileUpload(filesUrl);
-    };
+  const [filesUrl, setFilesUrl] = useState<string[]>([])
+  const [isDragging, setIsDragging] = useState(false)
+  const [uploadProgress, setUploadProgress] = useState<number | null>(null)
+  const [filePreviews, setFilePreviews] = useState<string[]>([])
 
-    /* Multipart/formdata Upload */
-    const handleFileUpload= async(file: File) => {
-      const form = new FormData();
-      form.append('file', file);
-      if(!!id && !!form) {
-        const response = await fetch(`/api/files?user_uuid=${id}`, {
-          method: 'POST',
-          body: form,
-        })
-        const responseData = await response.json();
-        setFilesUrl([...filesUrl, responseData?.publicUrl]);
-        // field.onChange(responseData?.publicUrl); // field value에 파일 정보 전달
-        // onFileUpload(responseData?.publicUrl); // 상위 컴포넌트로 파일 정보 전달
-      }
+  const handleDragEnter = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setIsDragging(true)
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setIsDragging(false)
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setIsDragging(false)
+
+    const files = e.dataTransfer.files
+    if (files.length > 0) {
+      uploadFiles(files)
+      previewFiles(files)
     }
+  }
 
-    useEffect(() => {
-      onFileUpload(filesUrl); // 상위 컴포넌트로 파일 정보 전달
-    },[filesUrl])
-  
-    return (
-        <div className={`grid grid-cols-2 grid-rows-auto gap-x-4 gap-y-3 px-2 py-1 mb-4 ${isDragging && 'shadow-[0_0px_0px_5px_rgba(135,211,248,0.5)_inset]'}`}>
-          <div
-            className={`size-[130px] bg-gray-50 border-[1px] rounded-md border-gray-300 p-4 text-center ${
-              isDragging ? "bg-gray-100" : ""
-            }`}
-            onDragEnter={handleDragEnter}
-            onDragOver={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
+  const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files && files.length > 0) {
+      uploadFiles(files)
+      previewFiles(files)
+    }
+  }
+
+  const uploadFiles = (files: FileList) => {
+    Array.from(files).forEach((file) => {
+      const totalSize = file.size
+      const chunkSize = 1024 * 1024
+      let uploaded = 0
+
+      const uploadInterval = setInterval(() => {
+        uploaded += chunkSize
+        const progress = (uploaded / totalSize) * 100
+        setUploadProgress(progress)
+
+        if (progress >= 100) {
+          clearInterval(uploadInterval)
+          setTimeout(() => {
+            setUploadProgress(null)
+          }, 500) // Delay for visual feedback
+        }
+        // onFileUpload(files);
+        handleFileUpload(file)
+      }, 500) // Simulated 500ms delay per chunk
+    })
+  }
+
+  const previewFiles = (files: FileList) => {
+    const previews: string[] = []
+    Array.from(files).forEach((file) => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        previews.push(reader.result as string)
+        setFilePreviews([...filePreviews, ...previews])
+      }
+      reader.readAsDataURL(file)
+    })
+  }
+
+  const handleDeletePreview = (index: number) => {
+    const updatedPreviews = [...filePreviews]
+    updatedPreviews.splice(index, 1)
+    setFilePreviews(updatedPreviews)
+
+    filesUrl.splice(index, 1)
+    onFileUpload(filesUrl)
+  }
+
+  /* Multipart/formdata Upload */
+  const handleFileUpload = async (file: File) => {
+    const form = new FormData()
+    form.append('file', file)
+    if (!!id && !!form) {
+      const response = await fetch(`/api/files?user_uuid=${id}`, {
+        method: 'POST',
+        body: form,
+      })
+      const responseData = await response.json()
+      setFilesUrl([...filesUrl, responseData?.publicUrl])
+      // field.onChange(responseData?.publicUrl); // field value에 파일 정보 전달
+      // onFileUpload(responseData?.publicUrl); // 상위 컴포넌트로 파일 정보 전달
+    }
+  }
+
+  useEffect(() => {
+    onFileUpload(filesUrl) // 상위 컴포넌트로 파일 정보 전달
+  }, [filesUrl])
+
+  useEffect(() => {
+    const urlsArr = imageUrls && imageUrls.split(',')
+    urlsArr && setFilePreviews(urlsArr)
+  }, [])
+
+  return (
+    <div
+      className={`grid grid-cols-2 grid-rows-auto gap-x-4 gap-y-3 px-2 py-1 mb-4 ${isDragging && 'shadow-[0_0px_0px_5px_rgba(135,211,248,0.5)_inset]'}`}
+    >
+      <div
+        className={`size-[130px] bg-gray-50 border-[1px] rounded-md border-gray-300 p-4 text-center ${
+          isDragging ? 'bg-gray-100' : ''
+        }`}
+        onDragEnter={handleDragEnter}
+        onDragOver={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <Input
+          id="file-input"
+          type="file"
+          {...props}
+          onChange={handleFileInputChange}
+          className="hidden"
+          multiple
+        />
+        <label
+          htmlFor={'file-input'}
+          className="cursor-pointer text-gray-500 size-full flex flex-col items-center justify-center"
+        >
+          <img src="/plus.svg" width="24" height="24" alt="plus icon" />
+        </label>
+      </div>
+      {filePreviews.map((preview, index) => (
+        <div
+          key={index}
+          className="size-[130px] relative flex flex-wrap p-2 border-[1px] rounded-md border-gray-300"
+        >
+          <img
+            src={preview}
+            alt={`File Preview ${index + 1}`}
+            className="size-[110px] aspect-square object-cover rounded-sm"
+          />
+          <Button
+            color="danger"
+            size="sm"
+            onClick={() => handleDeletePreview(index)}
+            className="absolute top-1 right-2 size-5 p-0 bg-slate-50"
           >
-            <Input
-              id="file-input"
-              type="file"
-              {...props}
-              onChange={handleFileInputChange}
-              className="hidden"
-              multiple
-            />
-            <label htmlFor={"file-input"}
-              className="cursor-pointer text-gray-500 size-full flex flex-col items-center justify-center"
-            >
-                <img src='/plus.svg' width='24' height='24' alt='plus icon' />
-            </label>
-          </div>
-            {filePreviews.map((preview, index) => (
-              <div key={index} className="size-[130px] relative flex flex-wrap p-2 border-[1px] rounded-md border-gray-300">
-                <img
-                  src={preview}
-                  alt={`File Preview ${index + 1}`}
-                  className="size-[110px] aspect-square object-cover rounded-sm"
-                />
-                <Button
-                  color="danger"
-                  size='sm'
-                  onClick={() => handleDeletePreview(index)}
-                  className="absolute top-1 right-2 size-5 p-0 bg-slate-50"
-                >
-                    <IoClose />
-                </Button>
-              </div>
-            ))}
+            <IoClose />
+          </Button>
         </div>
-    );
-};
+      ))}
+    </div>
+  )
+}
--- src/app/_components/Edit/ContentsInfo/ImagesContainer/index.tsx
+++ src/app/_components/Edit/ContentsInfo/ImagesContainer/index.tsx
@@ -1,99 +1,138 @@
 import React, { useEffect } from 'react'
 import styles from '../../edit.module.scss'
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { FileUpload } from './FileUpload';
-import { ContentsContainerProps } from '@/app/_types/contentsInfoTypes';
-import Accordion from '@/app/_components/common/accordion';
+import { RiDeleteBin6Line } from 'react-icons/ri'
+import { FileUpload } from './FileUpload'
+import { ContentsContainerProps } from '@/app/_types/contentsInfoTypes'
+import Accordion from '@/app/_components/common/accordion'
 
-const initialImageData = {
-  type: 'image',
-  layout: 'vertical',
-  ratio: 3/4,
-  urls: [] as string[],
-}
-
-export default function ImageContainer({id, onDelete, handleUpdateContent}: ContentsContainerProps) {
-  const [imageData, setImageData] = React.useState(initialImageData);
+export default function ImageContainer({
+  id,
+  content,
+  onDelete,
+  handleUpdateContent,
+}: ContentsContainerProps) {
+  const [imageData, setImageData] = React.useState({
+    type: 'image',
+    layout: content.layout || 'vertical',
+    ratio: content.ratio || 3 / 4,
+    urls: content.urls ? content.urls.split(',') : ([] as string[]), // 문자열을 배열로 변환
+  })
 
   const handleFileUpload = (files: any) => {
-    const fileUrls = Array.from(files).map((file: any) => file); // 파일 이름을 배열로 추가
-    // const updatedImageData = { ...imageData, urls: [...imageData.urls, ...fileUrls] }; // 기존 urls 배열에 파일 추가
-    setImageData({...imageData, urls: files});
-  };
+    const fileUrls = Array.from(files).map((file: any) => file) // 파일 이름을 배열로 추가
+    setImageData({ ...imageData, urls: [...imageData.urls, ...fileUrls] }) // 기존 urls 배열에 파일 추가
+  }
 
-  const handleChange = (e:any) => {
+  const handleChange = (e: any) => {
     setImageData({
       ...imageData,
-      [e.target.name]: e.target.value
+      [e.target.name.split('_')[0]]: e.target.value,
     })
   }
 
-   // 이미지 데이터가 변경될 때마다 상위 컴포넌트에 업데이트된 값을 전달
   useEffect(() => {
-    if (handleUpdateContent && imageData.urls.length > 0) {
-      // urls 배열을 쉼표로 구분된 문자열로 변환
-      const updatedContent = {
-        ...imageData,
-        urls: imageData.urls.join(','), // urls 배열을 그대로 유지
-      };
-      handleUpdateContent(updatedContent); // 상위로 업데이트된 데이터를 전달
+    const updatedContent = {
+      ...imageData,
+      urls: imageData.urls.length > 0 ? imageData.urls.join(',') : '',
     }
-    else {
-      const updatedContent = {...imageData, urls: ''}
-      handleUpdateContent(updatedContent)
-    }
-  }, [imageData]);
-
+    console.log(updatedContent)
+    handleUpdateContent(updatedContent) // 상위로 업데이트된 데이터를 전달
+  }, [imageData])
 
   return (
     <Accordion>
-        <Accordion.Header >{'이미지'}</Accordion.Header>
-        <Accordion.Animation>
-          <Accordion.Content description="초대장에 넣고 싶은 이미지를 추가해보세요">
-            {/* <ImageContainer /> */}
-            <div className='mt-4 pb-1'>
-                <p className='font-bold text-sm text-[#333] pb-1'>이미지 추가*</p>      
-                <FileUpload onFileUpload={handleFileUpload} />
-                <p className='font-bold text-sm text-[#333] pb-1'>레이아웃*</p>
-                <div className='flex gap-3 my-2'>
-                    <div className="flex flex-1 items-center space-x-1 ">
-                        <input type='radio' value="vertical" id="vertical" className={styles.radioItem} name='layout' onChange={handleChange} defaultChecked />
-                        <label htmlFor="vertical" onClick={handleChange} className={styles.label}>
-                          세로 나열형 
-                        </label>
-                    </div>
-                    <div className="flex flex-1 items-center space-x-1">
-                        <input type='radio' value="horizontal" id="horizontal" className={styles.radioItem} name='layout' onChange={handleChange}/>
-                        <label htmlFor="horizontal" onClick={handleChange} className={styles.label}>
-                          가로 나열형 </label>
-                    </div>
-                </div>      
-
-                <div className='flex gap-2 my-2' >
-                    <div className="flex flex-1 items-center space-x-1">
-                        <input type='radio' value="3/4" id="3/4" className={styles.radioItem} name='ratio' onChange={handleChange} defaultChecked />
-                        <label htmlFor="3/4" onClick={handleChange} className={styles.label}>
-                          3:4
-                        </label>
-                    </div>
-                    <div className="flex flex-1 items-center space-x-1">
-                        <input type='radio' value="1/1" id="1/1" className={styles.radioItem} name='ratio' onChange={handleChange}/>
-                        <label htmlFor="1/1" onClick={handleChange} className={styles.label}>
-                          1:1 </label>
-                    </div>
-                    <div className="flex flex-1 items-center space-x-1">
-                        <input type='radio' value="4/3" id="4/3" className={styles.radioItem} name='ratio' onChange={handleChange}/>
-                        <label htmlFor="4/3" onClick={handleChange} className={styles.label}>
-                          4:3 </label>
-                    </div>
-                </div>      
+      <Accordion.Header>{'이미지'}</Accordion.Header>
+      <Accordion.Animation>
+        <Accordion.Content description="초대장에 넣고 싶은 이미지를 추가해보세요">
+          <div className="mt-4 pb-1">
+            <p className="font-bold text-sm text-[#333] pb-1">이미지 추가*</p>
+            <FileUpload onFileUpload={handleFileUpload} imageUrls={content.urls} />
+            <p className="font-bold text-sm text-[#333] pb-1">레이아웃*</p>
+            <div className="flex gap-3 my-2">
+              <div className="flex flex-1 items-center space-x-1 ">
+                <input
+                  type="radio"
+                  name={`layout_${id}`}
+                  id="vertical"
+                  value="vertical"
+                  className={styles.radioItem}
+                  checked={imageData.layout === 'vertical'}
+                  onChange={handleChange} // input에만 onChange 이벤트 설정
+                />
+                <label htmlFor="vertical" className={styles.label}>
+                  세로 나열형
+                </label>
+              </div>
+              <div className="flex flex-1 items-center space-x-1">
+                <input
+                  type="radio"
+                  name={`layout_${id}`}
+                  id="horizontal"
+                  value="horizontal"
+                  className={styles.radioItem}
+                  checked={imageData.layout === 'horizontal'}
+                  onChange={handleChange}
+                />
+                <label htmlFor="horizontal" className={styles.label}>
+                  가로 나열형{' '}
+                </label>
+              </div>
             </div>
-            <button onClick={(e) => onDelete && onDelete(id)} className='relative bottom-0 left-[235px] flex items-center justify-center border-[1px] border-gray-300 rounded-sm w-[65px] p-1 my-2'>
-                <p>삭제</p>
-                <RiDeleteBin6Line className='size-4 text-gray-600 ml-1' />
-            </button>
-          </Accordion.Content>
-        </Accordion.Animation>
-      </Accordion>
+
+            <div className="flex gap-2 my-2">
+              <div className="flex flex-1 items-center space-x-1">
+                <input
+                  type="radio"
+                  name={`ratio_${id}`}
+                  id="ratio_3/4"
+                  value={3 / 4}
+                  className={styles.radioItem}
+                  checked={imageData.ratio == 3 / 4}
+                  onChange={handleChange}
+                />
+                <label htmlFor="ratio_3/4" className={styles.label}>
+                  3:4
+                </label>
+              </div>
+              <div className="flex flex-1 items-center space-x-1">
+                <input
+                  type="radio"
+                  name={`ratio_${id}`}
+                  id="ratio_1/1"
+                  value={1 / 1}
+                  className={styles.radioItem}
+                  checked={imageData.ratio == 1 / 1}
+                  onChange={handleChange}
+                />
+                <label htmlFor="ratio_1/1" className={styles.label}>
+                  1:1{' '}
+                </label>
+              </div>
+              <div className="flex flex-1 items-center space-x-1">
+                <input
+                  type="radio"
+                  name={`ratio_${id}`}
+                  id="ratio_4/3"
+                  value={4 / 3}
+                  className={styles.radioItem}
+                  checked={imageData.ratio == 4 / 3}
+                  onChange={handleChange}
+                />
+                <label htmlFor="ratio_4/3" className={styles.label}>
+                  4:3{' '}
+                </label>
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={(e) => onDelete && onDelete(id)}
+            className="relative bottom-0 left-[235px] flex items-center justify-center border-[1px] border-gray-300 rounded-sm w-[65px] p-1 my-2"
+          >
+            <p>삭제</p>
+            <RiDeleteBin6Line className="size-4 text-gray-600 ml-1" />
+          </button>
+        </Accordion.Content>
+      </Accordion.Animation>
+    </Accordion>
   )
 }
