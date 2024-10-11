09e755f9392a8864e78225da5bc76f6151a33e47
yunah <favoryun12@gmail.com>
2024-10-11 11:18:34 +0900

    Fix issues and build with vercel

--- src/app/(route)/invitation/[id]/page.tsx
+++ src/app/(route)/invitation/[id]/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import styles from '../edit.module.scss'
-import BaseInfo from '@/app/_components/edit/BasicInfo/index.tsx'
-import ContentsInfo from '@/app/_components/edit/ContentsInfo/index.tsx'
-import Background from '@/app/_components/edit/Background/index.tsx'
+import BaseInfo from '@/app/_components/edit/BasicInfo/index'
+import ContentsInfo from '@/app/_components/edit/ContentsInfo/index'
+import Background from '@/app/_components/edit/Background/index'
 import { Form } from '@/components/ui/form'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { useUser } from '@supabase/auth-helpers-react'
+import { ContentDataType } from '@/lib/types'
 
 const formSchema = z.object({
   user_id: z.string().min(10),
@@ -40,6 +42,10 @@ const formSchema = z.object({
 })
 
 const Edit = () => {
+  const data = useUser()
+  const [contentsInfo, setContentsInfo] = useState<ContentDataType[] | []>([])
+  const [checkedSlide, setCheckedSlide] = useState(null)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -78,10 +84,10 @@ const Edit = () => {
             <BaseInfo form={form} formSchema={formSchema} />
           </TabsContent>
           <TabsContent className="px-6 py-2" value="contents">
-            <ContentsInfo />
+            <ContentsInfo contentsInfo={contentsInfo} setContentsInfo={setContentsInfo} />
           </TabsContent>
           <TabsContent className="px-6 py-2" value="background">
-            <Background />
+            <Background form={form} checkedSlide={checkedSlide} setCheckedSlide={setCheckedSlide} />
           </TabsContent>
           {/* <button type="submit"  className='absolute top-[-5px] right-2 z-1000' >저장</button> */}
 
