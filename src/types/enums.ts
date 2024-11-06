caa0c7b70a5386e2960488e080b8b3e1c432b610
yunah <favoryun12@gmail.com>
2024-11-06 16:58:10 +0900

    refactoring/공통 Header 리팩토링

---
+++ src/types/enums.ts
@@ -0,0 +1,11 @@
+// 사용자 유형에 대한 Enum
+export enum UserType {
+  GUEST = 'guest',
+  MEMBER = 'member',
+}
+
+// 헤더 유형에 대한 Enum
+export enum HeaderType {
+  HOME = 'homeHeader',
+  ELSE = 'elseHeader',
+}
